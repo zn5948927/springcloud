46fe5b54d7a73a4c9db2aee372ab3d74da4d49e7
zuoning <16601037890@163.com>
2020-09-07 11:08:53 +0800

    cloud-gateway-gateway9527

---
+++ cloud-gateway-gateway9527/src/main/java/com/guigu/spring/GateWayMain9527.java
@@ -0,0 +1,20 @@
+package com.guigu.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @program: springcloud
+ * @description
+ * @author: Zuo Ning
+ * @create: 2020-09-03 17:09
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+
+}
