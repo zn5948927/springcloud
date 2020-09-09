eaf1cdf4c519ca02a885621dbed7b50ef1e445cf
zuoning <16601037890@163.com>
2020-09-09 17:50:35 +0800

    cloud-gateway-gateway9527 断言

---
+++ cloud-gateway-gateway9527/src/test/java/Time.java
@@ -0,0 +1,15 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @program: springcloud
+ * @description
+ * @author: Zuo Ning
+ * @create: 2020-09-07 16:56
+ **/
+public class Time {
+
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
