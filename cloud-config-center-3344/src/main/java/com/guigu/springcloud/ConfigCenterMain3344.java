fc95619fedb30b4755badb60a1069d151de80935
zuoning <16601037890@163.com>
2020-09-15 16:31:10 +0800

    cloud-config-center

---
+++ cloud-config-center-3344/src/main/java/com/guigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.guigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @program: springcloud
+ * @description
+ * @author: Zuo Ning
+ * @create: 2020-09-15 16:30
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
