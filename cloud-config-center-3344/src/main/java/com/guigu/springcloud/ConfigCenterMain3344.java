ee72e4f6380714f56f25a1719d76c801d45575fd
zuoning <16601037890@163.com>
2020-09-17 10:08:12 +0800

    ConfigCenterMain3344

--- cloud-config-center-3344/src/main/java/com/guigu/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/guigu/springcloud/ConfigCenterMain3344.java
@@ -13,7 +13,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigCenterMain3344 {
+
     public static void main(String[] args) {
         SpringApplication.run(ConfigCenterMain3344.class, args);
     }
+
 }
