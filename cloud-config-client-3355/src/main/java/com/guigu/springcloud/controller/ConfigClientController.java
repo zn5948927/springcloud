a6470521f1e95f560d799076910e65c2e9ba9860
zuoning <16601037890@163.com>
2020-09-17 17:14:39 +0800

    config-client3355

--- cloud-config-client-3355/src/main/java/com/guigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/guigu/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.guigu.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: Zuo Ning
  * @create: 2020-09-17 10:19
  **/
+@RefreshScope
 @RestController
 public class ConfigClientController {
 
