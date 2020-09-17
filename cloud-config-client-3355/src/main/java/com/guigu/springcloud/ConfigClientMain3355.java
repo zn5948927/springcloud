c096d7cffd520d271ea2947eefa0307d94d83462
zuoning <16601037890@163.com>
2020-09-17 10:23:06 +0800

    config-client3355

---
+++ cloud-config-client-3355/src/main/java/com/guigu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,18 @@
+package com.guigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: springcloud
+ * @description
+ * @author: Zuo Ning
+ * @create: 2020-09-17 10:19
+ **/
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
+
---
+++ cloud-config-client-3355/src/main/java/com/guigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.guigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: springcloud
+ * @description
+ * @author: Zuo Ning
+ * @create: 2020-09-17 10:19
+ **/
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
