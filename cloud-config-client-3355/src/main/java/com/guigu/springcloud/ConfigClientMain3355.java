d481140dcc66d32d8122fe0a4b25766a89193a24
zuoning <16601037890@163.com>
2020-09-17 10:45:26 +0800

    config-client3355

--- cloud-config-client-3355/src/main/java/com/guigu/springcloud/ConfigClientMain3355.java
+++ cloud-config-client-3355/src/main/java/com/guigu/springcloud/ConfigClientMain3355.java
@@ -2,6 +2,7 @@ package com.guigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @program: springcloud
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create: 2020-09-17 10:19
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigClientMain3355 {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientMain3355.class, args);
