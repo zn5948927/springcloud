c6b5a266ab93cc15e74f202260adc5daef840d4a
zuoning <16601037890@163.com>
2020-10-21 16:57:57 +0800

    cloud-stream-consumer8802 防重复消费

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/guigu/springcloud/StreamMQMain8803.java
@@ -0,0 +1,18 @@
+package com.guigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: springcloud
+ * @description
+ * @author: Zuo Ning
+ * @create: 2020-10-21 16:23
+ **/
+@SpringBootApplication
+public class StreamMQMain8803 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
