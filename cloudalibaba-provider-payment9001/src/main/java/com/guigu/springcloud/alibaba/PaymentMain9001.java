5247c67cbe5480798ccf5c9e0811a5c838826fb9
zuoning <16601037890@163.com>
2020-11-11 17:02:36 +0800

    cloudalibaba-provider-payment9001

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/guigu/springcloud/alibaba/PaymentMain9001.java
@@ -0,0 +1,19 @@
+package com.guigu.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @program: springcloud
+ * @description
+ * @author: Zuo Ning
+ * @create: 2020-11-11 15:55
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
---
+++ cloudalibaba-provider-payment9002/src/main/java/com/guigu/springcloud/alibaba/PaymentMain9002.java
@@ -0,0 +1,19 @@
+package com.guigu.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @program: springcloud
+ * @description
+ * @author: Zuo Ning
+ * @create: 2020-11-11 16:38
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class, args);
+    }
+}
