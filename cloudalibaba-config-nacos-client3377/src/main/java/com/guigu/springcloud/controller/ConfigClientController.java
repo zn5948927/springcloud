3c32b9fc49e65ba45071dc785b68e64774b056c9
zuoning <16601037890@163.com>
2020-11-12 16:00:36 +0800

    cloudalibaba-config-nacos-client3377

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/guigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.guigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: springcloud
+ * @description
+ * @author: Zuo Ning
+ * @create: 2020-11-12 15:50
+ **/
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
