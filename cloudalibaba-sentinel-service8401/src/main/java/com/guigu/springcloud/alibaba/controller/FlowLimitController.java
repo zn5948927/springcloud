a3e7bf52829cd6cae2f49701d552fe18df9c3977
zuoning <16601037890@163.com>
2020-11-16 17:27:22 +0800

    cloudalibaba-sentinel-service8401

--- cloudalibaba-sentinel-service8401/src/main/java/com/guigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/guigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.guigu.springcloud.alibaba.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @program: springcloud
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @create: 2020-11-16 11:02
  **/
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/testA")
@@ -19,7 +23,23 @@ public class FlowLimitController {
 
     @GetMapping("/testB")
     public String testB() {
-
+//        try {
+//            TimeUnit.MICROSECONDS.sleep(800);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        log.info(Thread.currentThread().getName() + "\t" + "....testB");
         return "------testB";
     }
+
+    @GetMapping("/testD")
+    public String testD() {
+        try {
+            TimeUnit.MICROSECONDS.sleep(800);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "------testD";
+    }
 }
+
