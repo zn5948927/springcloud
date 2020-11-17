5146c9c7941bd1a67b9619bf2f5b9f2719c70fa9
zuoning <16601037890@163.com>
2020-11-17 16:36:53 +0800

    cloudalibaba-sentinel-service8401

---
+++ cloud-api-commons/src/main/java/com/guigu/springcloud/myhandler/CustomerBlockHandler.java
@@ -0,0 +1,17 @@
+package com.guigu.springcloud.myhandler;
+
+import com.guigu.springcloud.entities.CommonResult;
+import com.sun.deploy.security.BlockedException;
+
+/**
+ * @program: springcloud
+ * @description
+ * @author: Zuo Ning
+ * @create: 2020-11-17 11:02
+ **/
+public class CustomerBlockHandler {
+
+    public static CommonResult handlerException(BlockedException exception) {
+        return new CommonResult(2020, "自定义限流信息。。。。");
+    }
+}
