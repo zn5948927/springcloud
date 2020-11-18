191736504d7ea669f17945f505ab1c7a890bfa82
zuoning <16601037890@163.com>
2020-11-18 10:26:13 +0800

    cloudalibaba-provider-payment9003/9004

---
+++ cloudalibaba-provider-payment9003/src/main/java/com/guigu/springcloud/alibaba/PaymentMain9003.java
@@ -0,0 +1,21 @@
+package com.guigu.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @program: springcloud
+ * @description
+ * @author: Zuo Ning
+ * @create: 2020-11-18 10:14
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9003
+{
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9003.class, args);
+    }
+}
+
